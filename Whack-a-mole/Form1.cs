6113fd588e4607d92e2b40d39221b9c9eef52f41
Frierk <Erik.friman@skola.sala.se>
2025-10-23 10:44:45 +0200

    Cleaned upp the code a little bit.

--- Whack-a-mole/Form1.cs
+++ Whack-a-mole/Form1.cs
@@ -95,15 +95,7 @@ namespace Whack_a_mole
         }
         private void timer2_Tick(object sender, EventArgs e)
         {
-            pictureBox1.Image = Resources.Mole_Down; pictureBox1.Enabled = false;
-            pictureBox2.Image = Resources.Mole_Down; pictureBox2.Enabled = false;
-            pictureBox3.Image = Resources.Mole_Down; pictureBox3.Enabled = false;
-            pictureBox4.Image = Resources.Mole_Down; pictureBox4.Enabled = false;
-            pictureBox5.Image = Resources.Mole_Down; pictureBox5.Enabled = false;
-            pictureBox6.Image = Resources.Mole_Down; pictureBox6.Enabled = false;
-            pictureBox7.Image = Resources.Mole_Down; pictureBox7.Enabled = false;
-            pictureBox8.Image = Resources.Mole_Down; pictureBox8.Enabled = false;
-            pictureBox9.Image = Resources.Mole_Down; pictureBox9.Enabled = false;
+            Mole_down();
             timer2.Stop();
             timer1.Start();
         }
@@ -195,15 +187,7 @@ namespace Whack_a_mole
             label1.Text = "Points: ";
             tid = 0;
             progressBar1.Value = 0;
-            pictureBox1.Image = Resources.Mole_Down; pictureBox1.Enabled = false;
-            pictureBox2.Image = Resources.Mole_Down; pictureBox2.Enabled = false;
-            pictureBox3.Image = Resources.Mole_Down; pictureBox3.Enabled = false;
-            pictureBox4.Image = Resources.Mole_Down; pictureBox4.Enabled = false;
-            pictureBox5.Image = Resources.Mole_Down; pictureBox5.Enabled = false;
-            pictureBox6.Image = Resources.Mole_Down; pictureBox6.Enabled = false;
-            pictureBox7.Image = Resources.Mole_Down; pictureBox7.Enabled = false;
-            pictureBox8.Image = Resources.Mole_Down; pictureBox8.Enabled = false;
-            pictureBox9.Image = Resources.Mole_Down; pictureBox9.Enabled = false;
+            Mole_down();
             button1.Visible = false;
             timer3.Start();
             timer1.Start();
@@ -226,6 +210,18 @@ namespace Whack_a_mole
                 progressBar1.Visible = true;
                 label3.Visible = true;
             }
+        }
+        private void Mole_down()
+        {
+            pictureBox1.Image = Resources.Mole_Down; pictureBox1.Enabled = false;
+            pictureBox2.Image = Resources.Mole_Down; pictureBox2.Enabled = false;
+            pictureBox3.Image = Resources.Mole_Down; pictureBox3.Enabled = false;
+            pictureBox4.Image = Resources.Mole_Down; pictureBox4.Enabled = false;
+            pictureBox5.Image = Resources.Mole_Down; pictureBox5.Enabled = false;
+            pictureBox6.Image = Resources.Mole_Down; pictureBox6.Enabled = false;
+            pictureBox7.Image = Resources.Mole_Down; pictureBox7.Enabled = false;
+            pictureBox8.Image = Resources.Mole_Down; pictureBox8.Enabled = false;
+            pictureBox9.Image = Resources.Mole_Down; pictureBox9.Enabled = false;
         }
     }
 }
